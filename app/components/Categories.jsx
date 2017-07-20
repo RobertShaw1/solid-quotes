9f75a3dcb0679021c18fafb38b239d8b2fd41a02
Robert Shaw <r.shaw1@yahoo.com>
2017-07-20 00:19:01 -0500

    Started draft of categories and rankings

---
+++ app/components/Categories.jsx
@@ -0,0 +1,12 @@
+//package dependicies
+const React = require('react');
+
+//local dependicies
+require('../index.css');
+
+
+export default Categories => (
+  <div className="ui text center aligned container">
+    <h2 className="ui center aligned categories-header">List of Categories</h2>
+  </div>
+)
