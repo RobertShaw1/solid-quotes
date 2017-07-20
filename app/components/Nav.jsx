a245cc7d5d63c95b1ff52ee8412e9c304ca7aac7
Robert Shaw <r.shaw1@yahoo.com>
2017-07-20 00:13:42 -0500

    Completed Nav Bar

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+//package dependicies
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+
+//NavBar Component
+export default class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeItem: 'Quote of the Day',
+    }
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <Menu color="red" inverted widths={3}>
+        <Menu.Item as={Link} to="/" name="Quote of the Day" active={activeItem === 'Quote of the Day'} onClick={this.handleItemClick} />
+
+        <Menu.Item as={Link} to="/categories" name="Categories" active={activeItem === 'Categories'} onClick={this.handleItemClick} />
+
+        <Menu.Item as={Link} to="/rankings" name="Rankings" active={activeItem === 'Rankings'} onClick={this.handleItemClick} />
+      </Menu>
+    )
+  }
+}
