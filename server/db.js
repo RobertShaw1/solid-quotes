0c22be5e92dfeeda579925e359c452b4602e3570
Robert Shaw <r.shaw1@yahoo.com>
2017-11-01 19:31:02 -0500

    updated exported db

--- server/db.js
+++ server/db.js
@@ -1,10 +1,15 @@
 
 const Sequelize = require('Sequelize')
 
-const db = new Sequelize('postgres://user:pass@example.com:5432/quotes');
+let db = new Sequelize('postgres://localhost:5432/quotes');
 
 // force: true will drop the table if it already exists
-db.sync({force: true}).then(() => {
+let options = {};
+if (process.env.SEEDING === "true") {
+  options = Object.assign({}, {force: true})
+}
+
+db.sync(options).then(() => {
   // Table created
   // return Quote.create({
   //   content: `“I want to change the world but they won’t give me the source code.”`,
