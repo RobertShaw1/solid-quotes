31aa860cd817e3f98d6f9184abd9c3e0047a9f36
Robert Shaw <r.shaw1@yahoo.com>
2018-06-13 22:20:48 -0500

    refactored seed file

--- seed.js
+++ seed.js
@@ -1,24 +1,48 @@
 
 const db = require('./server/db');
-const Quote = require('./server/models/quote');
+const { Quote } = require('./server/models');
 const quoteData = require('./quoteData');
 
-function seedQuotes() {
-  return Quote.bulkCreate(quoteData)
-    .catch(err => {
-    console.log('***error log***')
-    console.error(err);
-  })
+async function insertQuotes(data) {
+  try {
+    await Promise.all(data.map(quote => {
+      return Quote.create(
+        quote,
+        // {
+        //   include: [Category],
+        // }
+      )
+    }))
+    console.log('Finished Seeding!\n');
+    const allQuotes = await Quote.findAndCountAll();
+    console.log(`🎉  ${allQuotes.count} quotes seeded 🎉\n`);
+  } catch (error) {
+    console.log(`
+    \n*************************\n!!!ERROR WHILE SEEDING!!!\n*************************
+    `);
+    console.error(error.stack, '\n');
+    return error;
+  }
 }
 
-//this is the code to use when importing a synced db
-// db.then(seedQuotes)
-// .then(() => {
-//   db.close();
-// })
+function seed() {
+  return insertQuotes(quoteData)
+}
 
-// this is the code to use when importing an unsynced db
-db.sync({force: true}).then(seedQuotes)
-.then(() => {
+const main = async () => {
+  try {
+    console.log('Syncing db...');
+    await db.sync({ force: true })
+
+    console.log('Seeding database...');
+    await seed();
+
+  } catch (error) {
+    console.error(error.stack, '\n');
+    return error;
+  }
   db.close();
-})
+  return null;
+};
+
+main();
