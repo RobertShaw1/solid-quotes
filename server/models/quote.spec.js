a84a9011bd2a31a3aef83453cf858f856ebc6397
Robert Shaw <r.shaw1@yahoo.com>
2018-07-03 18:58:30 -0500

    refactored  quote spec and added test cases

--- server/models/quote.spec.js
+++ server/models/quote.spec.js
@@ -5,7 +5,7 @@ const should = chai.should()
 
 /* Local Modules */
 const db = require('../db')
-const { Quote } = require('./index')
+const { Author, Quote } = require('./index')
 
 describe('Quote', function() {
   let newQuote;
@@ -14,15 +14,28 @@ describe('Quote', function() {
     await db
       .sync({force: true})
       .then(async () => {
-        newQuote = await Quote.create({content: 'What is this?', attribution: 'Joshua'})
+        newQuote =
+          await Quote.create(
+            {text: 'What is this?', author: {name: 'Joshua'}},
+            {include: Author}
+          )
         return newQuote;
       })
   })
 
-  it('Quote should have content', function() {
-    expect(newQuote.content).to.equal('What is this?')
+  it('requires text', async function() {
+    try {
+      const badQuote = await Quote.create({attribution: 'Joshua'});
+      expect.fail()
+    } catch (error) {
+      expect(error.name).to.be.equal('SequelizeValidationError')
+      expect(error.errors[0].type).to.be.equal('notNull Violation')
+    }
   })
-  it('Quote should have authorId property', function() {
+  it('should have authorId property', function() {
     newQuote.dataValues.should.have.property('authorId');
+  })
+  it('should have categoryId property', function() {
+    newQuote.dataValues.should.have.property('categoryId');
   })
 })
